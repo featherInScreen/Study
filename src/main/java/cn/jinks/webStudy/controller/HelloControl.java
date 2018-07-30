7c784f8bb060aab3dc605f13982864cde6cf3619
featherInScreen <1324934837@qq.com>
2018-07-30 23:06:00 +0800

    commit

--- src/main/java/cn/jinks/webStudy/controller/HelloControl.java
+++ src/main/java/cn/jinks/webStudy/controller/HelloControl.java
@@ -1,14 +1,30 @@
 package cn.jinks.webStudy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloControl {
 
+    @Autowired
+    private HttpServletRequest request;
+
     @RequestMapping("home")
     public String sayHello(){
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        System.out.println(remoteAddr);
         return "hello";
     }
 }
